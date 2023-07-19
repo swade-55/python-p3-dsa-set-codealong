1202ca015a537a937d9bc18e1aaf0c9d3b9fd821
SamuelWade <samuelrcwade@gmail.com>
2023-07-19 12:57:59 -0500

    lab completed

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,19 @@
 class MySet:
-
-    pass
+    def __init__(self,enumerables=[]):
+        self.dictionary = {}
+        for value in enumerables:
+            self.dictionary[value] = True
+            
+    def has(self,value):
+        return value in self.dictionary
+    
+    def add(self,value):
+        self.dictionary[value] = True
+        return self
+    
+    def delete(self,value):
+        self.dictionary.pop(value,None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
